4d0eb02e35cb8ea89e23ff04f5f37f499252a295
AurelBouchard <au.bouchard@gmail.com>
2023-11-24 17:19:24 +0100

    BFS ok

--- step 5 BFS/laby_BFS.php
+++ step 5 BFS/laby_BFS.php
@@ -1,14 +1,15 @@
 <?php
 
-$map1 = [
+
+$map = [
     ["##","##","##","##","##","##","##","##","##","##","##"],
     ["##","  ","##","  ","  ","  ","  ","  ","  ","  ","##"],
     ["##","  ","##","  ","##","##","##","  ","##","##","##"],
     ["##","  ","##","  ","##","  ","  ","  ","  ","  ","##"],
     ["##","  ","  ","  ","  ","##","##","##","  ","  ","##"],
-    ["##","  ","##","  ","##","  ","  ","  ","  ","  ","##"],
+    ["##","  ","##","##","##","  ","  ","  ","  ","  ","##"],
     ["##","  ","##","  ","  ","  ","  ","##","  ","  ","##"],
-    ["##","##","##","##","  ","##","##","##","##","  ","##"],
+    ["##","  ","##","##","##","##","##","##","##","  ","##"],
     ["##","##","  ","  ","  ","##","##","##","##","##","##"],
     ["##","##","  ","##","##","##","##","##","##","  ","##"],
     ["##","##","  ","##","##","##","##","##","##","  ","##"],
@@ -43,9 +44,9 @@ $map2 = [
     ["##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##","##"],
 ];
 
-$map = [
+$map1 = [
     ["##","##","##","##","##","##","##","##","##","##","##","##","##","##"],
-    ["##","  ","  ","  ","  ","  ","  ","  ","##","  ","##","  ","  ","##"],
+    ["##","  ","##","  ","  ","  ","  ","  ","##","  ","##","  ","  ","##"],
     ["##","  ","##","##","  ","##","##","  ","##","  ","##","##","  ","##"],
     ["##","  ","##","  ","  ","  ","##","  ","##","  ","  ","##","  ","##"],
     ["##","  ","  ","  ","  ","  ","  ","  ","##","  ","  ","##","  ","##"],
@@ -59,25 +60,23 @@ $map = [
 
 
 // GAME VARIABLES
-const coordStart = [1,11]; // [y,x]
-const coordGoal = [8,10];
+const coordStart = [1,1]; // [y,x]
+const coordGoal = [7,9];
 
-const speed = 1*100000000; // ns
+const speed = 3*100000000; // ns
 
 const dir = ["S","E","N","W"];
 $indexDir = 0;
 $failures = 0;
 $playerCoord = coordStart;
 $goalFound = false;
-$step = 0;
-$cellId = 1;
+//$step = 0;
+$cellId = 0;
 $movedThisTurn = false;
 $teleporting = false;
 
-$listOfCrossroadsId = []; // crossroad = cellId
-$listOfCrossroadsCoord = []; // crossroad = COORD
-$allPath = [[coordStart]];
-//$currentPathIndex = 0;
+$cellsCoord = [coordStart]; // coord = [y,x]
+$segmentsList = [[$cellId]]; // segment = [cellId]
 
 
 // GAME FUNCTIONS
@@ -99,19 +98,25 @@ function showMap():void {
 }
 
 function showStep():void {
-    global $step, $cellId, $listOfCrossroadsId, $listOfCrossroadsCoord;
-    print_r("step: " . ($step) . "\n");
-    print_r( "list of crossroads id : " . join(', ', $listOfCrossroadsId )."\n");
+    global $cellId, $listOfCrossroadsId, $listOfCrossroadsCoord, $segmentsList;
+    print_r("step: " . ($cellId) . "\n");
+    //print_r( "list of crossroads id : " . join(', ', $listOfCrossroadsId )."\n");
 
-    $CRcoords = "";
-    foreach ($listOfCrossroadsCoord as $coord) {
-        $CRcoords .= "{".join(',',$coord)."} ";
+    print_r("segments : \n");
+
+    for ($i=0; $i<10; $i++) {
+        if (isset($segmentsList[$i])) {
+            print_r(join(' ', $segmentsList[$i] )."\n");
+        } else {
+            print_r("free space\n");
+        }
     }
-    print_r( "list of crossroads coord : " . $CRcoords ."\n");
-    print_r("cellId: " . ($cellId) . "\n");
+
     print_r("\n");
+    print_r("-----------------------------------------------------------------------------------------------------\n");
     print_r("\n");
-    print_r("\n");
+
+
 }
 
 
@@ -150,29 +155,31 @@ function checkIfGoalReached():bool {
     return ($playerCoord[0] === coordGoal[0] && $playerCoord[1] === coordGoal[1]);
 }
 
+function lastSegmentIndex():int {
+    global $segmentsList;
 
-function updatePaths($nextCellCoord):void {
-    global $cellId, $allPath, $playerCoord;
-
-/*    if ($cellId > count($path)) {
-        array_push($path, $playerCoord);
-    } else {
-        $path[$cellId-1] = $playerCoord;
-    }*/
-
-    $currentPathIndex = 0;
-
-    for ($i=0; $i<count($allPath); $i++) {
-        if (in_array($playerCoord, $allPath[$i])) {
-            $currentPathIndex = $i;
-            break;
+    $lowerLastSegmentCellId = 999;
+    $currentSegmentIndex = -1;
+    foreach ($segmentsList as $index => $segment) {
+        if ($segment[array_key_last($segment)] < $lowerLastSegmentCellId) {
+            $lowerLastSegmentCellId = $segment[array_key_last($segment)];
+            $currentSegmentIndex = $index;
         }
     }
+    return $currentSegmentIndex;
 
-    array_push($allPath[$currentPathIndex], $nextCellCoord);
 }
 
+function findSegmentEndingWith($cellId):int {
+    global $segmentsList;
 
+    foreach ($segmentsList as $index => $segment) {
+        if ($segment[array_key_last($segment)] === $cellId) {
+            return $index;
+        }
+    }
+    return -1;
+}
 
 function coordFromHere($dir) {
     global $playerCoord;
@@ -188,93 +195,183 @@ function coordFromHere($dir) {
     }
 }
 
-function teleportPlayer($destination) {
+function teleportPlayer($destination):void {
     global $map, $cellId, $playerCoord, $step;
+    //print_r("teleporting to : ".join(',',$destination)."\n");
 
-    $map[$playerCoord[0]][$playerCoord[1]] = sprintf("%02d", $cellId);
+    //$map[$playerCoord[0]][$playerCoord[1]] = sprintf("%02d", $cellId);
 
     $playerCoord = $destination;
 
     time_nanosleep(0,speed); // 0.1 sec
-    $step++;
     showMap();
     showStep();
-    $cellId++;
-
+    //$cellId++;
 }
 
 
-
+// security variables
 $w1 = 0;
 $w2 = 0;
 $w3 = 0;
 
-// include start cell in path
-updatePaths(coordStart);
 
+// main function variables
+$nextCellToGo = [];
+$map[$playerCoord[0]][$playerCoord[1]] = sprintf("%02d", $cellId);
 while (!$goalFound) {
 
-    $nextCellToGo = [];
+    //print_r("current coord : ".join(' ', $playerCoord )."\n");
+
     $indexDir = 0;
     $nOfWay = 0;
     $nOfDead = 0;
+    $newSegmentList = [];
     while ($indexDir < 4) {
+        //print_r("dir : ".dir[$indexDir]."\n");
 
         // shortcut to goal
-        if (checkIfGoalOnSigh(dir[$indexDir])) {
+/*        if (checkIfGoalOnSigh(dir[$indexDir])) {
+            print_r("GOAL ON SIGHT !\n");
             $nextCellToGo = coordFromHere(dir[$indexDir]);
+            print_r("nextCellToGo : ".join(' ', $nextCellToGo )."\n");
+            $nOfWay = -1;
             break;
-        }
+        }*/
 
         if (contentOfCellInDir(dir[$indexDir]) === '  ') {
-            // on a trouvé une case libre !
-            $nextCellToGo = coordFromHere(dir[$indexDir]);
+            // on a trouvé une voie
+            $nOfWay++;
 
-                // on a trouvé un carrefour !
-                $listOfCrossroadsId[] = $cellId;
-                $listOfCrossroadsCoord[] = $playerCoord;
+            // on crée un nouveau segment avec l'indice de cell suivant
+            $cellId++;
+            $newSegment = [$cellId]; // an array with only the first cell
+            $newSegmentList[] = $newSegment;
+            //print_r("new segment : ".join(' ', $newSegment )."\n");
 
-
-        } else {
-            // une case déjà visitée ou un mur
-            $nOfDead++;
-            if ($nOfDead === 4) {
-                print_r("impasse\n");
-                // on est dans une impasse ! => suppression du path
-                $nextCellToGo = $listOfCrossroadsCoord[array_key_last($listOfCrossroadsCoord)];
-
-                // je détruit ce path
-            }
+            // on met à jour la map et la liste des coordonnées
+            $coord = coordFromHere(dir[$indexDir]);
+            $map[$coord[0]][$coord[1]] = sprintf("%02d", $cellId);
+            $cellsCoord[] = $coord;
+            //print_r("new coord : ".join(' ', $coord )."\n");
         }
 
         $indexDir++;
     }
 
+    // si $nOfWay === 0 alors on est dans une impasse ! => suppression du segment
+    if ($nOfWay === 0) {
+        //print_r("DEAD END !\n");
 
-    updatePaths($nextCellToGo);
+        // TODO : change to slice + merge solution
+        //print_r("ccord : ".join(' ', $cellsCoord[$cellId] )."\n");
+        //print_r("segment : ".join(' ', $segmentsList[lastSegmentIndex()] )."\n");
+        //print_r("cellId : ".$cellId."\n");
+        //$segmentsList = array_splice($segmentsList, lastSegmentIndex(), 1);
+
+        $tempList = [];
+        foreach ($segmentsList as $index => $segment) {
+            if ($index !== lastSegmentIndex()) {
+                $tempList[] = $segment;
+            }
+        }
+        $segmentsList = $tempList;
+
+        //var_dump($segmentsList);
+    }
+
+    // si un seul segment, on l'agrège au segment en cours
+    if ($nOfWay === 1) {
+        //print_r("ONE WAY !\n");
+        //print_r($cellId . "\n");
+        //print_r("{ ".join(' ',$cellsCoord[$cellId]) . " }\n");
+
+        // met à jour les segments
+
+        //print_r("currentSegmentIndex : ".$currentSegmentIndex."\n");
+        $segmentsList[lastSegmentIndex()] = array_merge($segmentsList[lastSegmentIndex()], $newSegmentList[0]);
+        //var_dump($segmentsList) ;
+
+    }
+
+    // si plus que 1 segment => on ferme le segment en cours avec un marqueur de fin et on conserve les nouveaux segments
+    if ($nOfWay > 1) {
+        //print_r("CROSSROAD !\n");
+        //var_dump($segmentsList);
+        $segmentsList[lastSegmentIndex()][] = 999;
+
+
+        //var_dump($segmentsList);
+        foreach ($newSegmentList as $newSegment) {
+            //print_r("new segment : ".join(' ', $newSegment )."\n");
+            $segmentsList[] = $newSegment;
+        }
+        //var_dump($segmentsList);
+
+    }
+
+    if ($nOfWay > -1) {
+        // la position du curseur DOIT ETRE sur la plus petite fin de segment :
+        $lowerLastSegmentCellId = 999;
+        foreach ($segmentsList as $segment) {
+            if ($segment[array_key_last($segment)] < $lowerLastSegmentCellId) {
+                $lowerLastSegmentCellId = $segment[array_key_last($segment)];
+            }
+        }
+
+        //print_r("lowerLastSegmentCellId : ".$lowerLastSegmentCellId."\n");
+        $nextCellToGo = $cellsCoord[$lowerLastSegmentCellId];
+    }
+
     teleportPlayer($nextCellToGo);
-
 
     // but atteint ?
     if (checkIfGoalReached()) {
-        print_r("GOAL FOUND !\n");
-        print_r("\n");
+        //print_r("GOAL FOUND !\n");
+        //print_r("\n");
         $goalFound = true;
+        $segmentsList[findSegmentEndingWith(intval($map[$playerCoord[0]][$playerCoord[1]]))][] = 999;
     } else {
         $failures = 0;
     }
 
+    $nextCellToGo = [];
 }
 
 if ($goalFound) {
 
-    // format the path
-    foreach ($path as $key => $coord) {
-        $path[$key] = "{".join(',',$coord)."} ";
+    //print_r("last id :" . join('-', $playerCoord) . "\n");
+
+
+    $usedSegments = [];
+    // keep useful segments
+    for ($i=0; $i<count($segmentsList); $i++) {
+        if ($segmentsList[$i][array_key_last($segmentsList[$i])] === 999 ) {
+            array_pop($segmentsList[$i]) ;
+            $usedSegments[] = $segmentsList[$i];
+        }
     }
 
-    print_r("YOUR BEST PATH :\n");
-    print_r(join(' -> ', $path )."\n");
+    $headOfLastSegment = $segmentsList[findSegmentEndingWith(intval($map[$playerCoord[0]][$playerCoord[1]]))][0];
+
+    $pathList = [];
+    foreach ($usedSegments as $segment) {
+        if ($segment[0] <= $headOfLastSegment) {//$headOfLastSegment) {
+            $pathList[] = $segment;
+        }
+    }
+
+    $path = [];
+    for ($i=0; $i<count($pathList); $i++) {
+        for ($j=0; $j<count($pathList[$i]); $j++) {
+            $path[] = "{".join('-',$cellsCoord[$pathList[$i][$j]])."}";
+        }
+    }
+
+    print_r("path : ".join(' -> ', $path )."\n");
+
+
+
 
 } else {
     print_r("GAME OVER !\n");
@@ -282,3 +379,4 @@ if ($goalFound) {
 
 print_r("\n");
 print_r("------------------------------------------------------------------------\n");
+
